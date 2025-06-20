847ed9194dccc7b4f7d7a26b3986570924dcfd2d
Dhruv <dhruvr.purohit@gmail.com>
2025-06-20 15:30:42 +0530

    [CHECK]:first branch commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <div className="text-gray-600 bg-orange-400">
+        Hi hello
+      </div>
     </div>
+    
   )
 }
 
