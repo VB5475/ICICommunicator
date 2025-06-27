4aad08f00f0ff11e93e8e2ae5294b3733ba31444
Dhruv <dhruvr.purohit>
2025-06-27 14:34:40 +0530

    [FIX]:Make login screen responsive.

--- src/components/TypedText.jsx
+++ src/components/TypedText.jsx
@@ -5,14 +5,14 @@ import './TypedText.css';
 const TypedText = () => {
   const strings = [
     "Daring to be Different to win",
-    "Driven by Vision, Defined by Action",
-    "Dare to Begin."
+    // "Driven by Vision, Defined by Action",
+    // "Dare to Begin."
   ];
 
   const text = useTypingEffect(strings, 200, 1500);
 
   return (
-    <div className="h-12 w-full text-xl font-mono text-black bg-white p-4 flex justify-center items-center">
+    <div className="h-12 w-full text-xl font-mono text-black bg-white p-4 flex justify-center items-center ">
       {text}
       <span className="animate-pulse text-gray-600">|</span>
     </div>
--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import Logo from '../assets/images/logo.PNG'
 import TypedText from '@/components/TypedText'
-
+import '../App.css' 
 
 const Loginpage = () => {
     const [showPassword, setShowPassword] = useState(false)
@@ -26,29 +26,23 @@ const Loginpage = () => {
         })
     }
 
-    const ResetData = () =>{
-         setData({
-            id:'',
-            password:''
-        })
-    }
 
 
     
   return (
     <>
-        <div className='h-100vh w-screen flex '>
+        <div className='h-screen md:w-screen flex-row md:flex ' >
 
-            <div className='h-screen w-3/5 '>
+            <div className='sm:h-[50%] md:h-full sm:w-1/5 md:w-3/5 lg:w-3/5  '>
                 {/* Logo  */}
-                <div className='w-full h-[10%] flex ml-5 justify-start items-center'>
-                    <img src={Logo} alt="logo" className='h-19 w-15' />
-                    <span className='text-4xl text-blue-400 font-bold'>Communicator</span>
+                <div className='w-full h-[10%] flex ml-5 justify-start items-center mb-18 md:mb-[-70px] lg:mb-[-40px]'>
+                    <img src={Logo} alt="logo" className='h-20 w-15' />
+                    <span className='text-4xl text-blue-500 font-bold'>Communicator</span>
                 </div>
                 {/* writeing section */}
-                <div className='flex flex-col h-[90%] w-full justify-center items-center'>
+                <div className='flex flex-col h-[90%] w-full justify-center items-center mb-15 md:mb-0 lg:mb-0 '>
                     <div>
-                        <img src={Logo} alt="logo" className='h-20 w-15' />
+                        <img src={Logo} alt="logo" className='h-25 w-20 ' />
                     </div>
                     <div className='flex justify-center items-center'>
                         <span className='font-bold text-3xl text-blue-500'> Indian Chemical Industries </span>
@@ -59,20 +53,24 @@ const Loginpage = () => {
                 </div>
             </div>
 
-            <div className='h-screen w-2/5 flex justify-center items-center'>
+            <div className='sm:h-[70%] sm-w-[100%] md:h-full  sm:w-4/5 md:w-2/5 lg:w-2/5 flex justify-center items-center sm:mt-50 md:mt-0 '>
                  {/* Login Card */}
                 <form action="#" onSubmit={SaveData}>
-                    <Card className="backdrop-blur-sm bg-gray-200 border-2 border-slate-300 shadow-2xl shadow-black rounded-xl">
+                    <Card className=" w-70 backdrop-blur-sm bg-slate-100 border-2 border-white shadow-lg shadow-gray-400 rounded-md ml-8 md:ml-1">
                         <CardHeader>
-                            <CardTitle>Login Form </CardTitle>
+                            <CardTitle className='text-blue-500'>Login Form </CardTitle>
                         </CardHeader>
                             <CardContent className='flex flex-col justify-center items-center gap-6'>
                                 <div className="w-full">
-                                    <Label htmlFor="userId">User ID</Label>
+                                    <Label htmlFor="userId" className="flex justify-start items-center gap-2 mb-2">
+                                        <span> </span> User ID
+                                    </Label>
                                     <Input id="userId" placeholder="Enter your ID" value={data.id} onChange={(e) => setData({ ...data,id: e.target.value })}/>
                                 </div>
                                 <div className="w-full">
-                                    <Label htmlFor="password">Password</Label>
+                                    <Label htmlFor="password" className="flex justify-start items-center gap-2 mb-2">
+                                        <span> 🔒</span> Password
+                                    </Label>
                                     <Input type={showPassword ? "text" : "password"} id="password" placeholder="Enter your password" value={data.password} onChange={(e) => setData({ ...data,password: e.target.value })} />
                                     <div className='mt-4 w-full flex justify-start items-center'>
                                         <Checkbox className='border-2 border-black'
@@ -80,17 +78,15 @@ const Loginpage = () => {
                                         checked={showPassword}
                                         onCheckedChange={() => setShowPassword(!showPassword)}
                                         />
-                                        <Label htmlFor="" className='ml-4'>Show Password</Label>
+                                        <Label htmlFor="" className='ml-3'>Show Password</Label>
                                     </div>
                                 </div>
                                 
                                 <div className='w-full flex gap-4'>
-                                    <Button className="w-1/2 bg-blue-600 hover:bg-blue-800" type="submit">
+                                    <Button className="w-full bg-blue-600 hover:bg-blue-700" type="submit">
                                         Log In
                                     </Button>
-                                    <Button className='w-1/2 bg-blue-600 hover:bg-blue-800' type='reset' onClick={ResetData}>
-                                        cancel
-                                    </Button>
+                                    
                                 </div>
                             </CardContent>
                     </Card>
@@ -105,27 +101,3 @@ const Loginpage = () => {
 
 export default Loginpage
 
-// /Diccarde part
-
-{/* <div>
-                    <div className='flex justify-center items-center'>
-                        <img src={Logo} alt="not working" className='h-35 w-25'/>
-                    </div>
-                    <div className='flex flex-col justify-center items-center'>
-                        <span className='text-3xl font-bold'>INDIAN CHEMICAL INDUSTERIES</span>
-                        <span className='text-2xl'>Daring to be Different to win </span>
-                    </div>
-                </div> */}
-
-
-  {/* Header Section */}
-            {/* <div className='h-20 w-full bg-blue-500 flex items-center gap-10'>
-                <div className="ml-10 flex justify-center items-center gap-5">
-                    
-                </div>
-                <div className='flex flex-col'>
-                    <span className='text-3xl text-white font-bold'> </span>
-                    <span className='text-2xl text-white font-bold'>Communicator</span>
-                </div>
-            </div> */}
-            {/* logo part */}
