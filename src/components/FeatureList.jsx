6ac536b6a0265e6942ac94b4b3f2f63313cf8ded
Dhruv <dhruvr.purohit>
2025-07-04 11:19:33 +0530

    [REFACTOR]:remove padding from home header.

--- src/components/FeatureList.jsx
+++ src/components/FeatureList.jsx
@@ -71,7 +71,7 @@ const quickNavSection = allSections.find(s => s.title === "Quick Navigation");
 
 const FeatureList = () => (
     <>
-    <div className="w-full flex items-center justify-between px-5 py-3 border-b border-gray-100 bg-gray-50  mb-5">
+    <div className="w-full flex items-center justify-between px-5 py-3 border-b border-gray-100 bg-gray-50  mb-5 mt[-5px]">
         <span className="font-medium text-gray-800 text-[18px] md:text-[22px]">Home</span>
     </div>
     <div className="w-full max-w-5xl mx-auto px-2 lg:px-0 flex flex-col min-h-screen md:min-h-0 md:h-full">
